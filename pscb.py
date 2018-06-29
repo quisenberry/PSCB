50c926455b6f3cb15647d525927806573fa86ed4
Anon <anon@anon.com>
2018-06-29 10:56:22 -0400

    init mode test

--- pscb.py
+++ pscb.py
@@ -59,6 +59,7 @@ class PSCB:
         for pin in config.PIN_GROUP_OUTPUT:
             print("setting pin "+str(pin)+" as output")
             GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.output(pin, GPIO.LOW)
 
     def init_input(self):
         for pin in config.PIN_GROUP_INPUT:
