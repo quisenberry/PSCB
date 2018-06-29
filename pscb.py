4b4610b7f1fce553a62440ed8a6e734a968fbbee
Anon <anon@anon.com>
2018-06-29 10:04:36 -0400

    test push

--- pscb.py
+++ pscb.py
@@ -53,8 +53,11 @@ class PSCB:
     def init_pins(self):
         self.say('setting input output pins')
         for pin in config.PIN_GROUP_INPUT:
+            print("setting pin "+str(pin)+" as input")
             GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            
         for pin in config.PIN_GROUP_OUTPUT:
+            print("setting pin "+str(pin)+" as output")
             GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
             GPIO.output(pin, GPIO.LOW)
             print("state: "+GPIO.output(pin))
