93166f4642f93f983cb52655b822d1797b253c6e
Anon <anon@anon.com>
2018-06-30 12:53:01 -0400

    alpha 1.1

--- pscb.py
+++ pscb.py
@@ -116,40 +116,43 @@ class PSCB:
         Play (on the attached system sound device) the WAV file
         named wav_filename.
         '''
-
         try:
-            print
-            'Trying to play file ' + wav_filename
-            wf = wave.open(wav_filename, 'rb')
-        except IOError as ioe:
-            sys.stderr.write('IOError on file ' + wav_filename + '\n' + \
-                             str(ioe) + '. Skipping.\n')
-            return
-        except EOFError as eofe:
-            sys.stderr.write('EOFError on file ' + wav_filename + '\n' + \
-                             str(eofe) + '. Skipping.\n')
-            return
+            try:
+                print
+                'Trying to play file ' + wav_filename
+                wf = wave.open(wav_filename, 'rb')
+            except IOError as ioe:
+                sys.stderr.write('IOError on file ' + wav_filename + '\n' + \
+                                 str(ioe) + '. Skipping.\n')
+                return
+            except EOFError as eofe:
+                sys.stderr.write('EOFError on file ' + wav_filename + '\n' + \
+                                 str(eofe) + '. Skipping.\n')
+                return
 
-        # Instantiate PyAudio.
-        p = pyaudio.PyAudio()
+            # Instantiate PyAudio.
+            p = pyaudio.PyAudio()
 
-        # Open stream.
-        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                        channels=wf.getnchannels(),
-                        rate=wf.getframerate(),
-                        output=True, start=True)
+            # Open stream.
+            stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                            channels=wf.getnchannels(),
+                            rate=wf.getframerate(),
+                            output=True, start=True)
 
-        data = wf.readframes(chunk_size)
-        while len(data) > 0:
-            stream.write(data)
             data = wf.readframes(chunk_size)
+            while len(data) > 0:
+                stream.write(data)
+                data = wf.readframes(chunk_size)
 
-        # Stop stream.
-        stream.stop_stream()
-        stream.close()
+            # Stop stream.
+            stream.stop_stream()
+            stream.close()
 
-        # Close PyAudio.
-        p.terminate()
+            # Close PyAudio.
+            p.terminate()
+        except Exception as e:
+            print("unable to play "+wav_filename)
+            print(e)
 
     def say(self, text):
         self.sound.say(text)
