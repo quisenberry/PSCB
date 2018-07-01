c3057d225a113164d803acac2cad0909075e3c24
Anon <anon@anon.com>
2018-07-01 11:25:54 -0400

    alpha 1.3

--- deploy.py
+++ deploy.py
@@ -17,16 +17,47 @@ THE SOFTWARE.
 import config
 import time
 import git
-import sys
 import subprocess
 import pyttsx3
+import errno
+import os
+import sys
+
+
+PID = 0
+
+def pid_exists(pid):
+    """Check whether pid exists in the current process table.
+    UNIX only.
+    """
+    if pid < 0:
+        return False
+    if pid == 0:
+        # According to "man 2 kill" PID 0 refers to every process
+        # in the process group of the calling process.
+        # On certain systems 0 is a valid PID but we have no way
+        # to know that in a portable fashion.
+        raise ValueError('invalid PID 0')
+    try:
+        os.kill(pid, 0)
+    except OSError as err:
+        if err.errno == errno.ESRCH:
+            # ESRCH == No such process
+            return False
+        elif err.errno == errno.EPERM:
+            # EPERM clearly means there's a process to deny access to
+            return True
+        else:
+            # According to "man 2 kill" possible error values are
+            # (EINVAL, EPERM, ESRCH)
+            raise
+    else:
+        return True
 
 
 
 def main():
     #START PRIMARY SCRIPT
-    process = subprocess.Popen([sys.executable, config.DEPLOY_PATH])
-    print(process.pid)
 
     #INIT TEXT TO SPEECH
     engine = pyttsx3.init()
@@ -36,31 +67,42 @@ def main():
     engine.setProperty('rate', rate - 30)
 
     #LOOP FOR CHECKING IF GIT REPO HAS CHANGED
+    print(str(time.ctime())+": Checking for updates")
+
+    repo = git.Repo(config.PATH_ABSOLUT)
+    current_hash = repo.head.object.hexsha
+    print(current_hash)
+
+    try:
+        o = repo.remotes.origin
+        o.pull()
+        pull_hash = repo.head.object.hexsha
+    except Exception as e:
+        print("warning: unable to check repo, is internet connected?")
+        #print(e)
+        time.sleep(10)
+        pull_hash = current_hash
+    if current_hash != pull_hash:
+        print("changed")
+        engine.say('Please wait. I\'m updating the exhibit.')
+        engine.runAndWait()
+        process.kill()
+        process = subprocess.Popen([sys.executable, config.DEPLOY_PATH])
+    else:
+        print("repo is current")
+
+    process = subprocess.Popen([sys.executable, config.DEPLOY_PATH])
+    PID = process.pid
+
     while True:
-        print(str(time.ctime())+": Checking for updates")
-
-        repo = git.Repo(config.PATH_ABSOLUT)
-        current_hash = repo.head.object.hexsha
-        print(current_hash)
-
-        try:
-            o = repo.remotes.origin
-            o.pull()
-            pull_hash = repo.head.object.hexsha
-        except Exception as e:
-            print("warning: unable to check repo, is internet connected?")
-            #print(e)
-            time.sleep(10)
-            pull_hash = current_hash
-        if current_hash != pull_hash:
-            print("changed")
-            engine.say('Please wait. I\'m updating the exhibit.')
-            engine.runAndWait()
-            process.kill()
-            process = subprocess.Popen([sys.executable, config.DEPLOY_PATH])
+        if pid_exists(PID):
+            print("script is running")
         else:
-            print("repo is current")
-        time.sleep(config.GIT_POLL_RATE)
+            print("script has crashed, restarting")
+            process = subprocess.Popen([sys.executable, config.DEPLOY_PATH])
+            PID = process.pid
+        time.sleep(20)
+
 
 
 
